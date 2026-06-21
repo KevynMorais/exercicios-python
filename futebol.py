c477c0379a5c744174bc51989534e887fece3262
Kevyn Morais <kevynmorais721@gmail.com>
2026-06-21 17:48:01 -0300

    Melhoria do exercício de futebol
    Adicionado uma lista organizada com os dados de jogadores cadastrados e um sistema de levantamento de um jogador específico de acordo com o código de busca atribuído a ele.

--- futebol.py
+++ futebol.py
@@ -1,19 +1,39 @@
+jogadores = list()
 jogador = dict()
 gol = list()
-jogador['nome'] = str(input('Nome do Jogador: '))
-p = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
-for c in range (0, p):
-    g = int(input(f'Quantos gols na partida {c}? '))
-    gol.append(g)
-jogador['gols'] = gol[:]
-jogador['total'] = sum(gol)
+while True:
+    jogador.clear()
+    gol.clear()
+    jogador['nome'] = str(input('Nome do Jogador: '))
+    p = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+    for c in range (0, p):
+        g = int(input(f'Quantos gols na partida {c}? '))
+        gol.append(g)
+    jogador['gols'] = gol[:]
+    jogador['total'] = sum(gol)
+    jogadores.append(jogador.copy())
+    q = ' '
+    while q not in 'SN':
+        q = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if q == 'N':
+        break
 print('-='*30)
-print(jogador)
+print(f'{"cod"} {"nome":>10} {"gols":>14} {"total":>15}')
+for k, v in enumerate(jogadores):
+    print(f'{k:<10}', end = '')
+    for d in v.values():
+        print(f'{str(d):<15}', end = '')
+    print()
 print('-='*30)
-for k, v in jogador.items():
-    print(f'O campo {k} tem o valor {v}')
-print('-='*30)
-print(f'O jogador {jogador["nome"]} jogou {p} partidas.')
-for c in range (0, p):
-    print(f' => Na partida {c}, fez {jogador["gols"][c]} gols.')
-print(f'Foi um total de {jogador["total"]} gols.')
+while True:
+    b = int(input('Mostrar dados de qual jogador? (999 para parar) '))
+    if b == 999:
+        break
+    if b >= len(jogadores):
+        print(f'ERRO! Não existe jogador com o código {b}')
+    else:
+        print(f' - LEVANTAMENTO DO JOGADOR {jogadores[b]["nome"]}')
+        for i, g in enumerate(jogadores[b]['gols']):
+            print(f'    No jogo {i+1} fez {g} gols.')
+    print('-='*30)
+print('<< VOLTE SEMPRE >>')
