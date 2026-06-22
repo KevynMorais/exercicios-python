eeff688068c5845f977dc0b1570b98c82616cd32
Kevyn Morais <kevynmorais721@gmail.com>
2026-06-21 23:18:37 -0300

    Contador
    Neste código, temos um código que cria um contador inteligente. Primeiro, ele exemplifica uma contagem crescente e outra decrescente. Segundo, ao solicitar entrada de dados, de acordo com os dados, você cria uma contagem crescente ou decrescente e ela será exibido na tela tal como nas exemplificações!

---
+++ contador.py
@@ -0,0 +1,35 @@
+from time import sleep
+
+
+def contagem(inicio, fim, passo):
+    print('-=' * 30)
+    print(f'Contagem de {inicio} até {fim} de {passo} em {passo}.')
+    if inicio < fim:
+        for c in range(inicio, fim + 1, passo):
+            print(c, end = ' ')
+            sleep(0.5)
+    elif inicio > fim:
+        if passo > 0:
+            for c in range(inicio, fim - 1, passo * (-1)):
+                print(c, end=' ')
+                sleep(0.5)
+        elif passo < 0:
+            for c in range(inicio, fim - 1, passo):
+                print(c, end=' ')
+                sleep(0.5)
+    else:
+        print('ERRO! Início e fim iguais!')
+    print('FIM!')
+
+
+contagem(1, 10, 1)
+contagem(10, 0, 2)
+
+print('-='*30)
+print('Agora é a sua vez de personalizar a contagem!')
+i = int(input('Início: '))
+f = int(input('Fim:    '))
+p = int(input('Passo:  '))
+if p == 0:
+    p = 1
+contagem(i, f, p)
