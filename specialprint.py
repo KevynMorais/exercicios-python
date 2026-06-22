90e8b53be8cc4646fa64f359afeb34e0787ec8e3
Kevyn Morais <kevynmorais721@gmail.com>
2026-06-21 22:28:05 -0300

    Função de print de mensagens com linhas adaptáveis

---
+++ specialprint.py
@@ -0,0 +1,10 @@
+def escrever(msg):
+    tam = len(msg) + 4
+    print('~'*tam)
+    print(f'  {msg}')
+    print('~'*tam)
+    print()
+
+escrever('Gustavo Guanabara')
+escrever('Curso de Python no YouTube')
+escrever('CeV')
