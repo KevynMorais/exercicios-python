3e2ca6771d124429eee09d2873e62981b9e09026
Kevyn Morais <kevynmorais721@gmail.com>
2026-06-22 12:53:13 -0300

    Atualização do Contador
    Pequenas mudanças de animação do código.

--- contador.py
+++ contador.py
@@ -4,15 +4,16 @@ from time import sleep
 def contagem(inicio, fim, passo):
     print('-=' * 30)
     print(f'Contagem de {inicio} até {fim} de {passo} em {passo}.')
+    sleep(0.7)
     if inicio < fim:
         for c in range(inicio, fim + 1, passo):
             print(c, end = ' ')
-            sleep(0.5)
+            sleep(0.3)
     elif inicio > fim:
         if passo > 0:
             for c in range(inicio, fim - 1, passo * (-1)):
                 print(c, end=' ')
-                sleep(0.5)
+                sleep(0.3)
         elif passo < 0:
             for c in range(inicio, fim - 1, passo):
                 print(c, end=' ')
